d8720263b6b234e27aa4c881d2bf8f2e9274e44f
tzc1234 <peopli9997@gmail.com>
2023-07-03 11:48:58 +0100

    Delivers empty photos on error

--- PicsumAppTests/PhotoListViewModelTests.swift
+++ PicsumAppTests/PhotoListViewModelTests.swift
@@ -8,12 +8,22 @@
 import XCTest
 @testable import PicsumApp
 
-protocol PhotosLoader {
+struct Photo: Equatable {
     
 }
 
+protocol PhotosLoader {
+    func load() async throws -> [Photo]
+}
+
 class PhotoListViewModel {
+    var didLoad: (([Photo]) -> ())?
+    
     init(loader: PhotosLoader) {
+        
+    }
+    
+    func load() {
         
     }
 }
@@ -21,16 +31,45 @@ class PhotoListViewModel {
 final class PhotoListViewModelTests: XCTestCase {
     
     func test_init_withoutTriggerLoader() {
-        let loader = LoaderSpy()
-        _ = PhotoListViewModel(loader: loader)
+        let (_, loader) = makeSUT()
         
-        XCTAssertEqual(loader.messages.count, 0)
+        XCTAssertEqual(loader.stubs.count, 0)
+    }
+    
+    func test_load_deliversEmptyPhotosOnError() {
+        let (sut, _) = makeSUT(stubs: [.failure(NSError(domain: "error", code: 0))])
+        
+        var photos = [Photo]()
+        sut.didLoad = { photos = $0 }
+        
+        sut.load()
+        
+        XCTAssertEqual(photos, [])
     }
 
     // MARK: - Helpers
     
+    private typealias Result = Swift.Result<[Photo], Error>
+    
+    private func makeSUT(stubs: [Result] = []) -> (sut: PhotoListViewModel, loader: LoaderSpy) {
+        let loader = LoaderSpy(stubs: stubs)
+        let sut = PhotoListViewModel(loader: loader)
+        
+        return (sut, loader)
+    }
+    
     private class LoaderSpy: PhotosLoader {
+        private(set) var stubs: [Result]
+        
+        init(stubs: [Result]) {
+            self.stubs = stubs
+        }
+        
         private(set) var messages = [Any]()
+        
+        func load() async throws -> [Photo] {
+            try stubs.removeFirst().get()
+        }
     }
     
 }
