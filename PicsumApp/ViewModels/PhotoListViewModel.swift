b8519739b978ec49909e5071a4baeb57a875131c
tzc1234 <peopli9997@gmail.com>
2023-07-03 16:41:50 +0100

    Move PhotoListViewModel to production

---
+++ PicsumApp/ViewModels/PhotoListViewModel.swift
@@ -0,0 +1,70 @@
+//
+//  PhotoListViewModel.swift
+//  PicsumApp
+//
+//  Created by Tsz-Lung on 03/07/2023.
+//
+
+import Foundation
+
+final class PhotoListViewModel {
+    typealias Observer<T> = (T) -> Void
+    
+    var onLoad: Observer<Bool>?
+    var onError: Observer<String>?
+    var didLoad: Observer<[Photo]>?
+     
+    private var photos = [Photo]()
+    private var currentPage = 1
+    private var hasMorePage = true
+    private let loader: PhotosLoader
+    
+    init(loader: PhotosLoader) {
+        self.loader = loader
+    }
+    
+    func load() async {
+        resetCurrentPage()
+        
+        await loadPhotosFromLoader {
+            photos = $0
+            didLoad?(photos)
+        }
+    }
+    
+    func loadMore() async {
+        await loadPhotosFromLoader {
+            photos += $0
+            didLoad?(photos)
+        }
+    }
+    
+    private func resetCurrentPage() {
+        currentPage = 1
+    }
+    
+    private func loadPhotosFromLoader(completion: ([Photo]) -> Void) async {
+        guard hasMorePage else { return }
+        
+        onLoad?(true)
+        
+        do {
+            let photos = try await loader.load(page: currentPage)
+            updatePaging(by: photos)
+            completion(photos)
+        } catch {
+            onError?(Self.errorMessage)
+        }
+        
+        onLoad?(false)
+    }
+    
+    private func updatePaging(by photos: [Photo]) {
+        hasMorePage = !photos.isEmpty
+        currentPage += 1
+    }
+    
+    static var errorMessage: String {
+        "Error occured, please reload again."
+    }
+}
--- PicsumAppTests/PhotoListViewModelTests.swift
+++ PicsumAppTests/PhotoListViewModelTests.swift
@@ -8,68 +8,6 @@
 import XCTest
 @testable import PicsumApp
 
-class PhotoListViewModel {
-    typealias Observer<T> = (T) -> Void
-    
-    var onLoad: Observer<Bool>?
-    var onError: Observer<String>?
-    var didLoad: Observer<[Photo]>?
-     
-    private var photos = [Photo]()
-    private var currentPage = 1
-    private var hasMorePage = true
-    private let loader: PhotosLoader
-    
-    init(loader: PhotosLoader) {
-        self.loader = loader
-    }
-    
-    func load() async {
-        resetCurrentPage()
-        
-        await loadPhotosFromLoader {
-            photos = $0
-            didLoad?(photos)
-        }
-    }
-    
-    func loadMore() async {
-        await loadPhotosFromLoader {
-            photos += $0
-            didLoad?(photos)
-        }
-    }
-    
-    private func resetCurrentPage() {
-        currentPage = 1
-    }
-    
-    private func loadPhotosFromLoader(completion: ([Photo]) -> Void) async {
-        guard hasMorePage else { return }
-        
-        onLoad?(true)
-        
-        do {
-            let photos = try await loader.load(page: currentPage)
-            updatePaging(by: photos)
-            completion(photos)
-        } catch {
-            onError?(Self.errorMessage)
-        }
-        
-        onLoad?(false)
-    }
-    
-    private func updatePaging(by photos: [Photo]) {
-        hasMorePage = !photos.isEmpty
-        currentPage += 1
-    }
-    
-    static var errorMessage: String {
-        "Error occured, please reload again."
-    }
-}
-
 final class PhotoListViewModelTests: XCTestCase {
     
     func test_init_withoutTriggerLoader() {
