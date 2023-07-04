e8234457d45a5a801b58697121d99a72de78d125
tzc1234 <peopli9997@gmail.com>
2023-07-04 17:08:09 +0100

    fix typo

--- PicsumAppTests/Helpers/XCTestCase+Helpers.swift
+++ PicsumAppTests/Helpers/XCTestCase+Helpers.swift
@@ -12,8 +12,8 @@ extension XCTestCase {
     func makePhoto(id: String = "any id", author: String = "any author",
                    width: Int = 1, height: Int = 1,
                    webURL: URL = URL(string: "https://any-web-url.com")!,
-                   URL: URL = URL(string: "https://any-url.com")!) -> Photo {
-        .init(id: id, author: author, width: width, height: height, webURL: webURL, url: URL)
+                   url: URL = URL(string: "https://any-url.com")!) -> Photo {
+        .init(id: id, author: author, width: width, height: height, webURL: webURL, url: url)
     }
     
     func anyNSError() -> NSError {
