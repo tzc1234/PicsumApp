b62670e1de007e0944f3e9a79906a99e8571dd5d
tzc1234 <peopli9997@gmail.com>
2023-07-04 10:54:47 +0100

    Move PhotoListViewController to production

--- PicsumAppTests/PhotoListIntegrationTests.swift
+++ PicsumAppTests/PhotoListIntegrationTests.swift
@@ -9,47 +9,6 @@ import XCTest
 import UIKit
 @testable import PicsumApp
 
-class PhotoListViewController: UICollectionViewController {
-    private lazy var refreshControl = {
-        let refresh = UIRefreshControl()
-        refresh.addTarget(self, action: #selector(reloadPhotos), for: .valueChanged)
-        return refresh
-    }()
-    
-    private(set) var reloadPhotosTask: Task<Void, Never>?
-    private var viewModel: PhotoListViewModel?
-    
-    convenience init(viewModel: PhotoListViewModel) {
-        self.init(collectionViewLayout: UICollectionViewFlowLayout())
-        self.title = PhotoListViewModel.title
-        self.viewModel = viewModel
-    }
-    
-    override func viewDidLoad() {
-        collectionView.refreshControl = refreshControl
-        
-        setupBindings()
-        reloadPhotos()
-    }
-    
-    private func setupBindings() {
-        viewModel?.onLoad = { [weak self] isLoading in
-            if isLoading {
-                self?.refreshControl.beginRefreshing()
-            } else {
-                self?.refreshControl.endRefreshing()
-            }
-        }
-    }
-    
-    @objc private func reloadPhotos() {
-        reloadPhotosTask?.cancel()
-        reloadPhotosTask = Task {
-            await viewModel?.load()
-        }
-    }
-}
-
 final class PhotoListIntegrationTests: XCTestCase {
     
     func test_photosList_hasTitle() {
