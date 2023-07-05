6648a831edfec2ff3a5195308c72e480f7c626da
tzc1234 <peopli9997@gmail.com>
2023-07-05 13:54:32 +0100

    Move PhotoListViewController test helpers to separate file

---
+++ PicsumAppTests/Helpers/PhotoListViewController+TestHelpers.swift
@@ -0,0 +1,60 @@
+//
+//  PhotoListViewController+TestHelpers.swift
+//  PicsumAppTests
+//
+//  Created by Tsz-Lung on 05/07/2023.
+//
+
+import UIKit
+@testable import PicsumApp
+
+extension PhotoListViewController {
+    public override func loadViewIfNeeded() {
+        super.loadViewIfNeeded()
+        
+        collectionView.frame = CGRect(x: 0, y: 0, width: 1, height: 1)
+    }
+    
+    func simulateUserInitiatedReload() {
+        collectionView.refreshControl?.simulatePullToRefresh()
+    }
+    
+    var isShowingLoadingIndicator: Bool {
+        collectionView.refreshControl?.isRefreshing == true
+    }
+    
+    func numberOfRenderedPhotoView() -> Int {
+        collectionView.numberOfSections > photoViewSection ? collectionView.numberOfItems(inSection: photoViewSection) : 0
+    }
+    
+    func photoView(at item: Int) -> PhotoListCell? {
+        let ds = collectionView.dataSource
+        let indexPath = IndexPath(item: item, section: 0)
+        return ds?.collectionView(collectionView, cellForItemAt: indexPath) as? PhotoListCell
+    }
+    
+    @discardableResult
+    func simulatePhotoViewVisible(at item: Int) -> PhotoListCell? {
+        photoView(at: item)
+    }
+    
+    func simulatePhotoViewNotVisible(_ view: PhotoListCell, at item: Int) {
+        let d = collectionView.delegate
+        let indexPath = IndexPath(item: item, section: 0)
+        d?.collectionView?(collectionView, didEndDisplaying: view, forItemAt: indexPath)
+    }
+    
+    func simulatePhotoViewWillVisibleAgain(_ view: PhotoListCell, at item: Int) {
+        let d = collectionView.delegate
+        let indexPath = IndexPath(item: item, section: 0)
+        d?.collectionView?(collectionView, willDisplay: view, forItemAt: indexPath)
+    }
+    
+    private var photoViewSection: Int {
+        0
+    }
+    
+    func imageDataTask(at item: Int) -> Task<Void, Never>? {
+        imageDataTasks[.init(item: item, section: photoViewSection)]
+    }
+}
