6f9318f6776560cbf9c8aea5291ab95bdf18cb17
tzc1234 <peopli9997@gmail.com>
2023-07-06 16:12:04 +0100

    RemoteImageDataLoader passes correct url to client

--- PicsumAppTests/RemoteImageDataLoaderTests.swift
+++ PicsumAppTests/RemoteImageDataLoaderTests.swift
@@ -8,19 +8,47 @@
 import XCTest
 @testable import PicsumApp
 
-class RemoteImageDataLoader {
+class RemoteImageDataLoader: ImageDataLoader {
+    private let client: HTTPClient
+    
     init(client: HTTPClient) {
-        
+        self.client = client
+    }
+    
+    func loadImageData(from url: URL) async throws -> Data {
+        _ = try? await client.get(from: url)
+        return Data()
     }
 }
 
 final class RemoteImageDataLoaderTests: XCTestCase {
 
     func test_init_noTriggerClient() {
-        let client = ClientSpy(stubs: [])
-        let _ = RemoteImageDataLoader(client: client)
+        let (_, client) = makeSUT()
         
         XCTAssertEqual(client.loggedURLs.count, 0)
     }
+    
+    func test_loadImageData_passesCorrectURLToClient() async throws {
+        let (sut, client) = makeSUT(stubs: [.failure(anyNSError())])
+        let url = anyURL()
+        
+        _ = try await sut.loadImageData(from: url)
+        
+        XCTAssertEqual(client.loggedURLs, [url])
+    }
 
+    // MARK: - Helpers
+    
+    private func makeSUT(stubs: [ClientSpy.Stub] = [],
+                         file: StaticString = #filePath,
+                         line: UInt = #line) -> (sut: RemoteImageDataLoader, client: ClientSpy) {
+        let client = ClientSpy(stubs: stubs)
+        let sut = RemoteImageDataLoader(client: client)
+        
+        trackForMemoryLeaks(client, file: file, line: line)
+        trackForMemoryLeaks(sut, file: file, line: line)
+        
+        return (sut, client)
+    }
 }
