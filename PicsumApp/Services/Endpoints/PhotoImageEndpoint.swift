59606a3f1e3452c95b2430aa7164d7ae834e5939
tzc1234 <peopli9997@gmail.com>
2023-07-07 14:33:04 +0100

    Add PhotoImageEndpoint

---
+++ PicsumApp/Services/Endpoints/PhotoImageEndpoint.swift
@@ -0,0 +1,36 @@
+//
+//  PhotoImageEndpoint.swift
+//  PicsumApp
+//
+//  Created by Tsz-Lung on 07/07/2023.
+//
+
+import Foundation
+
+enum PhotoImageEndpoint {
+    case get(id: Int, width: UInt, height: UInt)
+    
+    var url: URL {
+        switch self {
+        case .get:
+            return makeURL()
+        }
+    }
+    
+    private var scheme: String { "https" }
+    private var baseURL: String { "picsum.photos" }
+    private var path: String {
+        switch self {
+        case let .get(id, width, height):
+            return "/id/\(id)/\(width)/\(height)"
+        }
+    }
+    
+    private func makeURL() -> URL {
+        var components = URLComponents()
+        components.scheme = scheme
+        components.host = baseURL
+        components.path = path
+        return components.url!
+    }
+}
---
+++ PicsumAppTests/PhotoImageEndpointTests.swift
@@ -0,0 +1,19 @@
+//
+//  PhotoImageEndpointTests.swift
+//  PicsumAppTests
+//
+//  Created by Tsz-Lung on 07/07/2023.
+//
+
+import XCTest
+@testable import PicsumApp
+
+final class PhotoImageEndpointTests: XCTestCase {
+
+    func test_get_deliverCorrectURL() {
+        let url = PhotoImageEndpoint.get(id: 99, width: 500, height: 500).url.absoluteString
+        
+        XCTAssertEqual(url, "https://picsum.photos/id/99/500/500")
+    }
+
+}
