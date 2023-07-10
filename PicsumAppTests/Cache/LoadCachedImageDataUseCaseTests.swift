7c3b352e32d6f901bc6972111c906151c3e0ea54
tzc1234 <peopli9997@gmail.com>
2023-07-10 15:10:21 +0100

    LocalImageDataLoader.loadImageData requests cached data with correct url

--- PicsumAppTests/Cache/LoadCachedImageDataUseCaseTests.swift
+++ PicsumAppTests/Cache/LoadCachedImageDataUseCaseTests.swift
@@ -10,23 +10,58 @@ import XCTest
 
 final class LoadCachedImageDataUseCaseTests: XCTestCase {
 
-    class LocalImageDataLoader {
+    class LocalImageDataLoader: ImageDataLoader {
+        private let store: ImageDataStoreSpy
+        
         init(store: ImageDataStoreSpy) {
-            
+            self.store = store
+        }
+        
+        func loadImageData(for url: URL) async throws -> Data {
+            store.retrieve(for: url)
+            throw anyNSError()
         }
     }
     
     func test_init_noTriggerStore() {
-        let store = ImageDataStoreSpy()
-        _ = LocalImageDataLoader(store: store)
+        let (_, store) = makeSUT()
         
         XCTAssertEqual(store.messages.count, 0)
     }
     
+    func test_loadImageData_requestsCachedDateForCorrectURL() async {
+        let (sut, store) = makeSUT()
+        let url = URL(string: "https://laod-image-url.com")!
+        
+        _ = try? await sut.loadImageData(for: url)
+        
+        XCTAssertEqual(store.messages, [.retrieve(url)])
+    }
+    
     // MARK: - Helpers
     
+    private func makeSUT(file: StaticString = #filePath,
+                         line: UInt = #line) -> (sut: LocalImageDataLoader, store: ImageDataStoreSpy) {
+        let store = ImageDataStoreSpy()
+        let sut = LocalImageDataLoader(store: store)
+        
+        trackForMemoryLeaks(store, file: file, line: line)
+        trackForMemoryLeaks(sut, file: file, line: line)
+        
+        return (sut, store)
+    }
+    
     class ImageDataStoreSpy {
-        private(set) var messages = [Any]()
+        private(set) var messages = [Message]()
+        
+        enum Message: Equatable {
+            case retrieve(URL)
+        }
+        
+        func retrieve(for url: URL) {
+            messages.append(.retrieve(url))
+        }
+        
     }
     
 }
