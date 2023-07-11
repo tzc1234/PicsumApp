9a992e1f49e36e4fb43f74d1d41dba061b97a01e
tzc1234 <peopli9997@gmail.com>
2023-07-11 11:13:21 +0100

    LocalImageDataLoader.init no message store in invalidate cached image data use case

---
+++ PicsumAppTests/Cache/InvalidateCachedImageDataUseCaseTests.swift
@@ -0,0 +1,33 @@
+//
+//  InvalidateCachedImageDataUseCaseTests.swift
+//  PicsumAppTests
+//
+//  Created by Tsz-Lung on 11/07/2023.
+//
+
+import XCTest
+@testable import PicsumApp
+
+final class InvalidateCachedImageDataUseCaseTests: XCTestCase {
+
+    func test_init_noTriggerStore() {
+        let (_, store) = makeSUT()
+        
+        XCTAssertEqual(store.messages.count, 0)
+    }
+
+    // MARK: - Helpers
+    
+    private func makeSUT(currentDate: @escaping () -> Date = Date.init,
+                         file: StaticString = #filePath,
+                         line: UInt = #line) -> (sut: LocalImageDataLoader, store: ImageDataStoreSpy) {
+        let store = ImageDataStoreSpy(retrieveStubs: [], deleteDataStubs: [], insertStubs: [])
+        let sut = LocalImageDataLoader(store: store, currentDate: currentDate)
+        
+        trackForMemoryLeaks(store, file: file, line: line)
+        trackForMemoryLeaks(sut, file: file, line: line)
+        
+        return (sut, store)
+    }
+    
+}
