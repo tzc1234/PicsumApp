3d8b51584891771782551c13c91ed7e47dd44dca
tzc1234 <peopli9997@gmail.com>
2023-07-12 17:21:51 +0100

    Move ContextDidSaveNotificationSpy to its own file

--- PicsumAppTests/Cache/CoreDataImageDataStoreTests.swift
+++ PicsumAppTests/Cache/CoreDataImageDataStoreTests.swift
@@ -195,22 +195,4 @@ final class CoreDataImageDataStoreTests: XCTestCase {
         }
     }
     
-    private class ContextDidSaveNotificationSpy {
-        private(set) var saveCount = 0
-        
-        init() {
-            NotificationCenter.default.addObserver(self, selector: #selector(contextDisSave),
-                                                   name: .NSManagedObjectContextDidSave,
-                                                   object: nil)
-        }
-        
-        @objc private func contextDisSave() {
-            saveCount += 1
-        }
-        
-        deinit {
-            NotificationCenter.default.removeObserver(self)
-        }
-    }
-    
 }
