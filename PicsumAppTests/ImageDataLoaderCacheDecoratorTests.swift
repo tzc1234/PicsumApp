f40620922d7b77da8866686b0d1e961d6209c3a5
tzc1234 <peopli9997@gmail.com>
2023-07-13 10:52:36 +0100

    ImageDataLoaderCacheDecorator.loadImageData cache data on loader success

--- PicsumAppTests/ImageDataLoaderCacheDecoratorTests.swift
+++ PicsumAppTests/ImageDataLoaderCacheDecoratorTests.swift
@@ -10,13 +10,17 @@ import XCTest
 
 class ImageDataLoaderCacheDecorator: ImageDataLoader {
     private let loader: ImageDataLoader
+    private let cache: ImageDataCache
     
-    init(loader: ImageDataLoader) {
+    init(loader: ImageDataLoader, cache: ImageDataCache) {
         self.loader = loader
+        self.cache = cache
     }
     
     func loadImageData(for url: URL) async throws -> Data {
-        try await loader.loadImageData(for: url)
+        let data = try await loader.loadImageData(for: url)
+        try await cache.save(data: data, for: url)
+        return data
     }
 }
 
@@ -54,17 +58,42 @@ final class ImageDataLoaderCacheDecoratorTests: XCTestCase {
         
         XCTAssertEqual(receivedData, data)
     }
+    
+    func test_loadImageData_cachesDataOnLoaderSuccess() async throws {
+        let data = anyData()
+        let cache = CacheSpy()
+        let (sut, _) = makeSUT(stubs: [.success(data)], cache: cache)
+        let url = anyURL()
+        
+        _ = try await sut.loadImageData(for: url)
+        
+        XCTAssertEqual(cache.cachedData, [.init(data: data, url: url)])
+    }
 
     // MARK: - Helpers
     
     private func makeSUT(stubs: [RemoteImageDataLoaderSpy.Stub] = [],
+                         cache: CacheSpy = .init(),
                          file: StaticString = #filePath,
                          line: UInt = #line) -> (sut: ImageDataLoaderCacheDecorator, loader: RemoteImageDataLoaderSpy) {
         let loader = RemoteImageDataLoaderSpy(stubs: stubs)
-        let sut = ImageDataLoaderCacheDecorator(loader: loader)
+        let sut = ImageDataLoaderCacheDecorator(loader: loader, cache: cache)
         trackForMemoryLeaks(loader, file: file, line: line)
         trackForMemoryLeaks(sut, file: file, line: line)
         return (sut, loader)
+    }
+    
+    private class CacheSpy: ImageDataCache {
+        private(set) var cachedData = [Cached]()
+        
+        func save(data: Data, for url: URL) async throws {
+            cachedData.append(.init(data: data, url: url))
+        }
+        
+        struct Cached: Equatable {
+            let data: Data
+            let url: URL
+        }
     }
     
 }
