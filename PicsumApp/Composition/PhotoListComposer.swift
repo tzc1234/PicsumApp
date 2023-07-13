8ddf3f9f28de080ee4fbf4e99e1ee41246895415
tzc1234 <peopli9997@gmail.com>
2023-07-13 12:08:48 +0100

    Remove image downsample helper

--- PicsumApp/Composition/PhotoListComposer.swift
+++ PicsumApp/Composition/PhotoListComposer.swift
@@ -16,42 +16,11 @@ enum PhotoListComposer {
                 let viewModel = PhotoImageViewModel(
                     photo: photo,
                     imageLoader: imageLoader,
-                    imageConverter: ImageDownsampleConverter.downsample)
+                    imageConverter: UIImage.init)
                 return PhotoListCellController(viewModel: viewModel)
             })
         }
         
         return viewController
-    }
-}
-
-enum ImageDownsampleConverter {
-    // https://medium.com/@zippicoder/downsampling-images-for-better-memory-consumption-and-uicollectionview-performance-35e0b4526425
-    static func downsample(from data: Data) -> UIImage? {
-        // Create an CGImageSource that represent an image
-        let imageSourceOptions = [kCGImageSourceShouldCache: false] as CFDictionary
-        guard let imageSource = CGImageSourceCreateWithData(data as CFData, imageSourceOptions) else {
-            return nil
-        }
-        
-        // Calculate the desired dimension
-        let scale = UIScreen.main.scale
-        let pointSize = UIScreen.main.bounds.size
-        let width: CGFloat = pointSize.width / 2
-        let maxDimensionInPixels = width * scale
-        
-        // Perform downsampling
-        let downsampleOptions = [
-            kCGImageSourceCreateThumbnailFromImageAlways: true,
-            kCGImageSourceShouldCacheImmediately: true,
-            kCGImageSourceCreateThumbnailWithTransform: true,
-            kCGImageSourceThumbnailMaxPixelSize: maxDimensionInPixels
-        ] as [CFString: Any] as CFDictionary
-        guard let downsampledImage = CGImageSourceCreateThumbnailAtIndex(imageSource, 0, downsampleOptions) else {
-            return nil
-        }
-        
-        // Return the downsampled image as UIImage
-        return UIImage(cgImage: downsampledImage)
     }
 }
