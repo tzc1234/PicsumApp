acd18c5780232e07a490caf4475855b0a6ce1dcc
tzc1234 <peopli9997@gmail.com>
2023-07-15 09:55:05 +0100

    Clean up SceneDelegate

--- PicsumApp/SceneDelegate.swift
+++ PicsumApp/SceneDelegate.swift
@@ -9,6 +9,19 @@ import CoreData
 import UIKit
 
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+    private lazy var client = URLSessionHTTPClient(session: .shared)
+    private lazy var remoteImageDataLoader = RemoteImageDataLoader(client: client)
+    
+    private let storeURL = NSPersistentContainer.defaultDirectoryURL().appending(path: "data-store.sqlite")
+    private lazy var imageDataStore = {
+        try? CoreDataImageDataStore(storeURL: storeURL)
+    }()
+    private lazy var localImageDataLoader: LocalImageDataLoader? = {
+        guard let store = imageDataStore else { return nil }
+        
+        return LocalImageDataLoader(store: store)
+    }()
+    
     var window: UIWindow?
 
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
@@ -19,25 +32,24 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     }
     
     func configureWindow() {
-        let client = URLSessionHTTPClient(session: .shared)
         let photosLoader = RemotePhotosLoader(client: client)
-        let remoteImageDataLoader = RemoteImageDataLoader(client: client)
-        
-        let storeURL = NSPersistentContainer.defaultDirectoryURL().appending(path: "data-store.sqlite")
-        let imageDataStore = try! CoreDataImageDataStore(storeURL: storeURL)
-        let localImageDataLoader = LocalImageDataLoader(store: imageDataStore)
-        
-        let imageDataLoader = ImageDataLoaderWithFallbackComposite(
-            primary: localImageDataLoader,
-            fallback: ImageDataLoaderCacheDecorator(
-                loader: remoteImageDataLoader,
-                cache: localImageDataLoader))
-        
-        let photoImageLoader = PhotoImageDataLoaderAdapter(imageDataLoader: imageDataLoader)
+        let photoImageLoader = PhotoImageDataLoaderAdapter(imageDataLoader: makeImageLoader())
         let viewModel = PhotoListViewModel(loader: photosLoader)
         let photoListViewController = PhotoListComposer.composeWith(viewModel: viewModel, imageLoader: photoImageLoader)
         
         window?.rootViewController = UINavigationController(rootViewController: photoListViewController)
         window?.makeKeyAndVisible()
+    }
+    
+    private func makeImageLoader() -> ImageDataLoader {
+        guard let localImageDataLoader else {
+            return remoteImageDataLoader
+        }
+        
+        return ImageDataLoaderWithFallbackComposite(
+            primary: localImageDataLoader,
+            fallback: ImageDataLoaderCacheDecorator(
+                loader: remoteImageDataLoader,
+                cache: localImageDataLoader))
     }
 }
