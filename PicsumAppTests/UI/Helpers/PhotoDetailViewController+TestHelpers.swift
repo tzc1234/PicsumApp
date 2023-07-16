0f9423bc32ff381d316219701767ec597224a6b9
tzc1234 <peopli9997@gmail.com>
2023-07-16 14:36:54 +0100

    Move PhotoDetailViewController to production

--- PicsumAppTests/UI/Helpers/PhotoDetailViewController+TestHelpers.swift
+++ PicsumAppTests/UI/Helpers/PhotoDetailViewController+TestHelpers.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+@testable import PicsumApp
 
 extension PhotoDetailViewController {
     func layoutIfNeeded() {
--- PicsumAppTests/UI/PhotoDetailIntegrationTests.swift
+++ PicsumAppTests/UI/PhotoDetailIntegrationTests.swift
@@ -8,54 +8,6 @@
 import XCTest
 @testable import PicsumApp
 
-class PhotoDetailViewController: UIViewController {
-    private(set) lazy var authorLabel = UILabel()
-    private(set) lazy var webURLLabel = UILabel()
-    private(set) lazy var imageView = UIImageView()
-    private(set) lazy var reloadButton = {
-        let btn = UIButton()
-        btn.addTarget(self, action: #selector(loadImage), for: .touchUpInside)
-        return btn
-    }()
-    
-    private(set) var task: Task<Void, Never>?
-    private(set) var isLoading = false
-    
-    private let photo: Photo
-    private let imageDataLoader: ImageDataLoader
-    
-    init(photo: Photo, imageDataLoader: ImageDataLoader) {
-        self.photo = photo
-        self.imageDataLoader = imageDataLoader
-        super.init(nibName: nil, bundle: nil)
-        self.title = "Photo"
-    }
-    
-    required init?(coder: NSCoder) { nil }
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        authorLabel.text = photo.author
-        webURLLabel.text = photo.webURL.absoluteString
-        loadImage()
-    }
-    
-    @objc private func loadImage() {
-        reloadButton.isHidden = true
-        isLoading = true
-        task = Task {
-            do {
-                imageView.image = UIImage(data: try await imageDataLoader.loadImageData(for: photo.url))
-                reloadButton.isHidden = true
-            } catch {
-                reloadButton.isHidden = false
-            }
-
-            isLoading = false
-        }
-    }
-}
-
 final class PhotoDetailIntegrationTests: XCTestCase {
 
     func test_init_hasTitle() {
