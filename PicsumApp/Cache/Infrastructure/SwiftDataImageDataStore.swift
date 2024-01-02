9003880ba6a63b2829c1a876a7ddeb70b94e57ab
tzc1234 <peopli9997@gmail.com>
2024-01-02 14:33:44 +0000

    Move SwiftDataImageDataStore to prod

---
+++ PicsumApp/Cache/Infrastructure/SwiftDataImageDataStore.swift
@@ -0,0 +1,51 @@
+//
+//  SwiftDataImageDataStore.swift
+//  PicsumApp
+//
+//  Created by Tsz-Lung on 02/01/2024.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class SwiftDataImage {
+    var data: Data
+    var timestamp: Date
+    @Attribute(.unique) var url: String
+    
+    init(data: Data, timestamp: Date, url: URL) {
+        self.data = data
+        self.timestamp = timestamp
+        self.url = url.absoluteString
+    }
+}
+
+final actor SwiftDataImageDataStore: ModelActor, ImageDataStore {
+    let modelContainer: ModelContainer
+    let modelExecutor: ModelExecutor
+    private let modelContext: ModelContext
+    
+    init(configuration: ModelConfiguration) throws {
+        self.modelContainer = try ModelContainer(for: SwiftDataImage.self, configurations: configuration)
+        self.modelContext = ModelContext(modelContainer)
+        self.modelExecutor = DefaultSerialModelExecutor(modelContext: modelContext)
+    }
+    
+    func retrieveData(for url: URL) throws -> Data? {
+        let predicate = #Predicate<SwiftDataImage> { $0.url == url.absoluteString }
+        var descriptor = FetchDescriptor<SwiftDataImage>(predicate: predicate)
+        descriptor.fetchLimit = 1
+        return try modelContext.fetch(descriptor).first?.data
+    }
+    
+    func insert(data: Data, timestamp: Date, for url: URL) throws {
+        modelContext.insert(SwiftDataImage(data: data, timestamp: timestamp, url: url))
+        try modelContext.save()
+    }
+    
+    func deleteAllData(until date: Date) throws {
+        let predicate = #Predicate<SwiftDataImage> { $0.timestamp <= date }
+        try modelContext.delete(model: SwiftDataImage.self, where: predicate)
+    }
+}
