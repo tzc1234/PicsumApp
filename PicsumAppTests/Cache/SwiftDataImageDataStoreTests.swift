c0359ce90b9b214bf3c2db4f3c9f40d0947596f3
tzc1234 <peopli9997@gmail.com>
2024-01-02 13:37:09 +0000

    SwiftDataImageDataStore.retrieveData delivers cached data

--- PicsumAppTests/Cache/SwiftDataImageDataStoreTests.swift
+++ PicsumAppTests/Cache/SwiftDataImageDataStoreTests.swift
@@ -9,13 +9,37 @@ import XCTest
 import SwiftData
 @testable import PicsumApp
 
-final class SwiftDataImageDataStore: ImageDataStore {
+@Model
+final class SwiftDataImage {
+    var data: Data
+    var timestamp: Date
+    var url: URL
+    
+    init(data: Data, timestamp: Date, url: URL) {
+        self.data = data
+        self.timestamp = timestamp
+        self.url = url
+    }
+}
+
+actor SwiftDataImageDataStore: ImageDataStore {
+    private let container: ModelContainer
+    private let context: ModelContext
+    
+    init(configuration: ModelConfiguration) throws {
+        self.container = try ModelContainer(for: SwiftDataImage.self, configurations: configuration)
+        self.context = ModelContext(container)
+    }
+    
     func retrieveData(for url: URL) async throws -> Data? {
-        nil
+        var descriptor = FetchDescriptor<SwiftDataImage>()
+        descriptor.fetchLimit = 1
+        return try context.fetch(descriptor).first?.data
     }
     
     func insert(data: Data, timestamp: Date, for url: URL) async throws {
-        
+        context.insert(SwiftDataImage(data: data, timestamp: timestamp, url: url))
+        try context.save()
     }
     
     func deleteAllData(until date: Date) async throws {
@@ -43,11 +67,43 @@ final class SwiftDataImageDataStoreTests: XCTestCase {
         XCTAssertNil(lastRetrievedData)
     }
     
+    func test_retrievesData_deliversCachedData() async throws {
+        let sut = try makeSUT()
+        let url = anyURL()
+        let data = anyData()
+        
+        try await insert(data: data, url: url, to: sut)
+        let retrievedData = try await sut.retrieveData(for: url)
+        
+        XCTAssertEqual(retrievedData, data)
+    }
+    
     // MARK: - Helpers
     
     private func makeSUT(file: StaticString = #filePath, line: UInt = #line) throws -> ImageDataStore {
-        let sut = SwiftDataImageDataStore()
+        let sut = try SwiftDataImageDataStore(configuration: .init(isStoredInMemoryOnly: true))
         trackForMemoryLeaks(sut, file: file, line: line)
         return sut
+    }
+    
+    private func insert(data: Data, timestamp: Date = Date(), url: URL, to sut: ImageDataStore,
+                        file: StaticString = #filePath, line: UInt = #line) async throws {
+        let notificationSpy = ContextDidSaveNotificationSpy()
+        
+        try await sut.insert(data: data, timestamp: timestamp, for: url)
+        
+        XCTAssertTrue(notificationSpy.saveCount > 0, "Expect at least save once", file: file, line: line)
+    }
+    
+    private struct DataInput {
+        let data: Data
+        let date: Date
+        let url: URL
+        
+        init(data: Data, date: Date = Date(), url: URL) {
+            self.data = data
+            self.date = date
+            self.url = url
+        }
     }
 }
